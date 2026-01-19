e272e7f9fcf0115c52f05e2957b8a99ff29be04d
GREEN YANG <grrrrin93@gmail.com>
2026-01-19 22:55:46 +0900

    배포 설정 최종 수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   // 1. 타입스크립트 에러 무시 (배포 성공을 위해)
   typescript: {
     ignoreBuildErrors: true,
